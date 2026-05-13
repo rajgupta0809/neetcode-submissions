e4fad1341beaf60aa48b3e777ef964ae1e2f2928
Raj Gupta <89046317+rajgupta0809@users.noreply.github.com>
2026-05-14 01:18:20 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.length();
+        int m = s2.length();
+        sort(s1.begin(), s1.end());
+
+        for(int i=0;i<m;i++){
+            int j = i+n;
+            string sub = s2.substr(i, n);
+
+            cout<<sub<<endl;
+
+            sort(sub.begin(), sub.end());
+
+            if(sub == s1) return true;
+        }
+        return false;
+    }
+};
